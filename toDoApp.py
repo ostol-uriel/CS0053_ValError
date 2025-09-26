af6cfa120d3e60f854b94990d6dc313988fa2c88
XanderDacillo <202311326@fit.edu.ph>
2025-09-26 10:50:11 +0800

    Fixed Logic na ito pars

--- toDoApp.py
+++ toDoApp.py
@@ -1,39 +1,44 @@
-# toDoApp.py
+tasks = []
 
-tasks=[]
+def add_task(task):
+    tasks.append(task)
+    print("Task added!")
 
-def addtask(task) :
-  tasks.append(task)
-  print("task added!")
-
-def showTasks( ):
-    if len(tasks)==0 :
-      print("no tasks yet")
+def show_tasks():
+    if not tasks:
+        print("No tasks yet")
     else:
-     for i in range (len(tasks)):
-      print(i+1,".",tasks[i])
+        for i, task in enumerate(tasks, 1):
+            print(f"{i}. {task}")
 
-def removetask(tasknumber):
-    tasks.pop(tasknumber) 
-    print("task removed!!")
+def remove_task(task_number):
+    if 0 <= task_number < len(tasks):
+        tasks.pop(task_number)
+        print("Task removed!")
+    else:
+        print("Invalid task number!")
 
 def main():
     while True:
-        print("1 Add Task")
-        print("2.Show Tasks")
-        print("3.Remove Task")
-        print("4- Exit")
-        ch = input("enter choice : ")
-        if ch=="1":
-            t = input("enter task : ")
-            addtask(t)
-        elif ch=="2":
-            showTasks()
-        elif ch=="3":
-            n=int(input("enter task no to remove: "))
-            removetask(n)   
-        elif ch=="4":
-            break;
+        print("1. Add Task")
+        print("2. Show Tasks")
+        print("3. Remove Task")
+        print("4. Exit")
+        choice = input("Enter choice: ")
+        if choice == "1":
+            task = input("Enter task: ")
+            add_task(task)
+        elif choice == "2":
+            show_tasks()
+        elif choice == "3":
+            try:
+                num = int(input("Enter task number to remove: ")) - 1
+                remove_task(num)
+            except ValueError:
+                print("Please enter a valid number!")
+        elif choice == "4":
+            break
         else:
-            print("wrong choice!!")
+            print("Wrong choice!")
+
 main()
